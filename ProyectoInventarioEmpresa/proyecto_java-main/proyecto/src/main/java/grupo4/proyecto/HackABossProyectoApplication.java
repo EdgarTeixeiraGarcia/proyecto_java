5349012e06e9aa805ba09b8f03b4f61d3598caef
Thorquios <36553632+Thorquios@users.noreply.github.com>
2023-09-14 17:55:52 +0200

    Add files via upload

---
+++ ProyectoInventarioEmpresa/proyecto_java-main/proyecto/src/main/java/grupo4/proyecto/HackABossProyectoApplication.java
@@ -0,0 +1,13 @@
+package grupo4.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackABossProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackABossProyectoApplication.class, args);
+	}
+
+}
